db10ca95753c702b30b45fbfb32c0226961dc62c
friaf <shovel.irena@gmail.com>
2023-05-03 23:26:15 +0000

    add correct allowed hosts in settings to deploy

--- smorrebrod/settings.py
+++ smorrebrod/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gastrobar-smorrebrod.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['smorrebrod-gastrobar.herokuapp.com', 'localhost']
 
 
 # Application definition
